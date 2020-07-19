145748a8b8bb48acd982d4419466e50af1e8f0d6
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-07-19 23:41:44 +0530

    Another Variation of the Cyberpunk button in ReactJS

--- styled-web/src/App.js
+++ styled-web/src/App.js
@@ -20,6 +20,64 @@ function App() {
   clip-path: polygon(92% 0, 100% 25%, 100% 100%, 8% 100%, 0% 75%, 0 0);
 `
 
+  const Button = styled.button`
+  --yellow-color: #fcee09; 
+  --red-color: #ff003c;
+  --black-color: #050a0e;
+  --blue-color: #00f0ff;
+  --white-color: #fafafa;
+
+  width: 230px;
+  height: 60px; 
+  border: 0;
+  outline: none;
+  background-color: var(--black-color);
+  cursor: pointer;
+  position: relative;
+  font-family: Tomorrow, sans-serif;
+  font-size: .85rem;
+  text-transform: uppercase;
+  color: var(--black-color);
+  clip-path: polygon(92% 0, 100% 25%, 100% 100%, 8% 100%, 0% 75%, 0 0);
+  z-index: 1;
+`
+
+const Button_Content = styled.div`
+--yellow-color: #fcee09; 
+--red-color: #ff003c;
+--black-color: #050a0e;
+--blue-color: #00f0ff;
+--white-color: #fafafa;
+
+display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  right: 2px;
+  bottom: 2px;
+  background-color: var(--yellow-color);
+  clip-path: polygon(92% 0, 100% 25%, 100% 100%, 8% 100%, 0% 75%, 0 0);
+  z-index: 2;
+`
+const Button_Label = styled.span`
+--yellow-color: #fcee09; 
+--red-color: #ff003c;
+--black-color: #050a0e;
+--blue-color: #00f0ff;
+--white-color: #fafafa;
+
+height: 10px;
+  font-size: .40rem;
+  position: absolute;
+  bottom: -4px;
+  right: 8%;
+  padding: 0 5px;
+  background-color: var(--yellow-color);
+  z-index: 3;
+`
+
   return (
     <div className="App">
       <header className="App-header">
@@ -27,15 +85,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <ClipPathButton>CyberPunk 2077</ClipPathButton>
+        <br></br>
+        <Button>
+          <Button_Content>
+            The future is now_
+          </Button_Content>
+          <Button_Label>r25</Button_Label>
+        </Button>
       </header>
     </div>
   );
